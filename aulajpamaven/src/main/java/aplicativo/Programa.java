6fa8a17ee0ad6f635ec45fbf2ce8a5f3a27357ba
JoaopedroHZN <joaopedromontelo164@gmail.com>
2026-05-25 10:18:57 -0300

    Entendendo JPA/Hibernate

---
+++ aulajpamaven/src/main/java/aplicativo/Programa.java
@@ -0,0 +1,11 @@
+package aplicativo;
+
+import dominio.Pessoa;
+
+
+public class Programa {
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa(1,"Joao Pedro","joao@gmail.com");
+        System.out.println(p1);
+    }
+}
---
+++ aulajpamaven/src/main/java/dominio/Pessoa.java
@@ -0,0 +1,56 @@
+package dominio;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String email, String nome) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
